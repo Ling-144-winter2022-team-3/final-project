fdb1253c09ad63d4da9c3c42c4aaaea4f34893f8
ColinHirschberg <97074574+ColinHirschberg@users.noreply.github.com>
2022-03-04 11:49:33 -0800

    Create read_csv_col_of_sents.py

---
+++ data-analysis/read_csv_col_of_sents.py
@@ -0,0 +1,12 @@
+#This script reads the column under the header "sentences" from dataset1.csv and the column titled "sentences" from dataset2.csv into two lists and concatenates thee lists together.
+
+#--- statement in the commandlines -----
+
+# python script_name.py ~/filepath/file_1.txt ~/filepath/file_2.txt
+
+#python read_csv_col_to_sents.
+
+#----------------------------------------
+
+
+#Please copy and paste the code between the rows of hyphens into a single line in the Repl.it shell/terminal in order to run this program.
